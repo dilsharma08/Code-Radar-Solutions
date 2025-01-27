e1a66cd8e119c1f9ab71fcbe500e5b95e880b780
dilsharma08 <dil0134.becse24@chitkara.edu.in>
2025-01-28 00:14:30 +0530

    Solution to Check Divisibility

---
+++ Check Divisibility@@6788dd63490637cfc44e1b91/code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int a;
+    printf("%d",&a);
+    if(a%2==0 && year%11==0){
+        printf("Divisible");
+    }
+    else{
+        printf("Not Divisible");
+    }
+}

4866e43daedc2f2fc6c9d8a927d8e66ad2efe0b7
dilsharma08 <dil0134.becse24@chitkara.edu.in>
2025-02-04 00:41:52 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int a,b ;
+    char res;
+    scanf("%d %d %c",&a,&b,&c);
+    switch(res){
+    case '+':
+    int sum =a+b;
+    printf("%d",sum);
+break;
+
+case '-':
+int sub =a-b ;
+printf("%d",sub);
+break ;
+
+case '*':
+int mul=a*b;
+printf("%d",mul);
+break ;
+
+case '/':
+div=a/b;
+printf("%d",div);
+break;
+   }
+}

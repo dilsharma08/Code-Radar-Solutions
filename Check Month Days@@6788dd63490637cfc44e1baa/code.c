da7827881c6ff484a7744bbb441e140856982fdc
dilsharma08 <dil0134.becse24@chitkara.edu.in>
2025-02-15 11:26:45 +0530

    Solution to Check Month Days

--- Check Month Days@@6788dd63490637cfc44e1baa/code.c
+++ Check Month Days@@6788dd63490637cfc44e1baa/code.c
@@ -7,16 +7,20 @@ int main(){
     }
     case 4: case 6: case 9: case 11:
     printf("30");
+    break;
     case 1: case 3:case 5:case 7:case 8:case 10:case 12:
     printf("31");
     case 2:
     if((month%4==0 && month%100!=0)||(month%400==0)){
         printf("29");
+      
 
     }
     else{
         printf("28");
     }
+    break;
     default:
     printf("Invalid month");
+    break;
 }
